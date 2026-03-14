d2d57b1eef0a8ada3fd6fbf779a9634fee2243ad
Ihor Kasatkin <igorkasat22@gmail.com>
2026-03-14 13:35:45 +0100

    Pipeline fix.

--- src/test/java/org/mini_payment_service/paymentservice/PaymentServiceApplicationTests.java
+++ src/test/java/org/mini_payment_service/paymentservice/PaymentServiceApplicationTests.java
@@ -2,8 +2,10 @@ package org.mini_payment_service.paymentservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PaymentServiceApplicationTests {
 
     @Test
